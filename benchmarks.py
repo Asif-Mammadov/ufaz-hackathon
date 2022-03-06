3ed422a5a2ea01c02d6f2c29834fe502fa953da9
Asif-Mammadov <asif.mammad@hotmail.com>
2022-03-06 12:02:26 +0400

    refactored

--- benchmarks.py
+++ benchmarks.py
@@ -4,20 +4,29 @@ def ackley(X, a=20, b=0.2, c=2*np.pi):
   # https://www.sfu.ca/~ssurjano/Code/ackleyr.html
   d = len(X)
   sum1 = np.sum(X ** 2)
-  sum2 = np.sum(np.cos(c * X))
+  sum2 = np.sum(np.cos(c * X.astype('float')))
 
   term1 = -a * np.exp(-b * np.sqrt(sum1/d))
   term2 = - np.exp(sum2/d)
 
   y = term1 + term2 + a + np.exp(1)
-  return 1
+  return y
 
 def rastrigin(X):
   # https://www.sfu.ca/~ssurjano/Code/rastrr.html
   d = len(X)
-  sum = np.sum(X ** 2 - 10 * np.cos(2 * np.pi * X))
+  sum = np.sum(X ** 2 - 10 * np.cos(2 * np.pi * X.astype('float')))
   y = 10 * d + sum
   return y
+
+# def rosenbrock_test(X):
+#   # https://www.sfu.ca/~ssurjano/Code/rosenr.html
+#   X = X.astype('float')
+#   d = len(X)
+#   xi = X[1:d-1]
+#   xnext = X[2:d]
+#   sum = np.sum(100 * (xnext - xi ** 2) ** 2 + (xi-1) ** 2 )
+#   return sum
 
 def rosenbrock(args):
   x, y = args
@@ -28,6 +37,6 @@ def rosenbrock(args):
 def schwefel(X):
   # https://www.sfu.ca/~ssurjano/Code/schwefr.html
   d = len(X)
-  sum = np.sum(X * np.sin(np.sqrt(np.abs(X))))
+  sum = np.sum(X * np.sin(np.sqrt(np.abs(X.astype('float')))))
   y = 418.9829 * d - sum
   return y
--- main_plot.py
+++ main_plot.py
@@ -1,13 +1,15 @@
 import benchmarks
+import matplotlib.pyplot as plt
 from gsa import GSA
+
 
 # X = np.random.rand(100, 2) * 300
 a = GSA(benchmark=benchmarks.ackley, iter_num=20, distance=10, pop_size=20)
-# avg, bests = a.run()
 
-# fig, ax = plt.subplots(2)
+avg, bests = a.run()
 
-# ax[0].plot(avg, label='avg')
-# ax[1].plot(bests, label='best')
-# plt.show()
-a.animate()
+fig, ax = plt.subplots(2)
+
+ax[0].plot(avg, label='avg')
+ax[1].plot(bests, label='best')
+plt.show()
